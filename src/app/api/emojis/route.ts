41c10632461296ad604a1faea13754d4cc753e2a
Ujjawal Kumar <workwithujjawal@gmail.com>
2025-09-15 21:31:37 +0530

    fixed caching behaviour

--- src/app/api/emojis/route.ts
+++ src/app/api/emojis/route.ts
@@ -1,13 +1,21 @@
 import { db } from "@/src/db/drizzle";
 import { emoji } from "@/src/db/schema";
-
-export const dynamic = "force-static";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
     // fetch the emojis from DB.
     const result = await db.select().from(emoji);
-    return Response.json({ status: "ok", data: result });
+    return NextResponse.json(
+      { status: "ok", data: result },
+      {
+        headers: {
+          "Cache-Control": "no-store, must-revalidate",
+          "CDN-Cache-Control": "no-store",
+          "Vercel-CDN-Cache-Control": "no-store",
+        },
+      }
+    );
   } catch (err) {
     console.log("error = ", err);
     return Response.json({ status: "error", data: err });
